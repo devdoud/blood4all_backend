81d961cb107e0b7dfbffffc9656e02e9ffaa1d03
EtuDoud <bouraimawadoud@gmail.com>
2023-06-20 16:44:16 +0100

    changing the prismadb path in signin

--- pages/api/auth/doctor/signin.js
+++ pages/api/auth/doctor/signin.js
@@ -1,4 +1,4 @@
-import { prisma } from "../../../lib/prismadb"
+import { prisma } from '@/lib/prismadb';
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { serialize } from "cookie";
