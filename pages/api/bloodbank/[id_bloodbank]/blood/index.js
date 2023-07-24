9ade123a8b6d4fa25d57f87bdebd0ff3992efc1c
EtuDoud <bouraimawadoud@gmail.com>
2023-07-24 20:14:23 +0100

    change post blood bag

--- pages/api/bloodbank/[id_bloodbank]/blood/index.js
+++ pages/api/bloodbank/[id_bloodbank]/blood/index.js
@@ -1,25 +1,24 @@
-
 import { prisma } from '@/lib/prismadb';
 
 export default async function handler(req, res) {
-    const method = req.method
+    const method = req.method;
 
     switch (method) {
         case 'GET':
             try {
-                const bloods = await prisma.blood.findMany()
-                res.status(200).json({bloods})
+                const bloods = await prisma.blood.findMany();
+                res.status(200).json({ bloods });
             } catch (error) {
-                res.status(404).json({message: 'Desole impossible de recuperer les sang'});
+                res.status(404).json({ message: 'Desole impossible de recuperer les sang' });
             } finally {
-                await prisma.$disconnect
+                await prisma.$disconnect();
             }
-
             break;
-        
-        case 'POST': 
-            const { bloodGroupe, quantity, typeproduit } = req.body ;
-            
+
+        case 'POST':
+            const { bloodGroupe, quantity, typeproduit } = req.body;
+            console.log(method);
+
             try {
                 const blood = await prisma.blood.create({
                     data: {
@@ -27,18 +26,18 @@ export default async function handler(req, res) {
                         quantity: quantity,
                         productType: typeproduit
                     }
-                })
+                });
 
-                res.status(200).json({blood})
-
+                res.status(200).json({ blood });
             } catch (error) {
-                res.status(404).json({message: "desole votre requette ne peut point aboutir"})
+                res.status(404).json({ message: "desole votre requette ne peut point aboutir" });
             } finally {
-                await prisma.$disconnect
+                await prisma.$disconnect();
             }
-    
+            break;
+
         default:
-            res.status(401).json({message: `La methode ${methode} n'est pas autorisee !`})
+            res.status(401).json({ message: `La methode ${method} n'est pas autorisee !` });
             break;
     }
 }
