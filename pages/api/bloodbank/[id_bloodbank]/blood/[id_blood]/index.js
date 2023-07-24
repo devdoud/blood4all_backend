1cc82816f7c16d45dd9a709517bcc62893715071
EtuDoud <bouraimawadoud@gmail.com>
2023-07-24 20:21:28 +0100

    change update blood bag and delete

--- pages/api/bloodbank/[id_bloodbank]/blood/[id_blood]/index.js
+++ pages/api/bloodbank/[id_bloodbank]/blood/[id_blood]/index.js
@@ -1,34 +1,29 @@
-
-
-import { prisma } from "../../../../../../lib/prismadb"
+import { prisma } from '@/lib/prismadb';
 
 export default async function handler(req, res) {
-    const method = req.method
+    const method = req.method;
 
-    console.log(req.query.id_blood)
+    console.log(req.query.id_blood);
 
     switch (method) {
         case 'GET':
-            
             try {
                 const blood = await prisma.blood.findUnique({
                     where: {
                         id: req.query.id_blood
                     }
-                })
+                });
 
-                res.status(200).json({blood})
+                res.status(200).json({ blood });
             } catch (error) {
-                res.status(404).json({message: 'impossible de recuperer ce sang'})
+                res.status(404).json({ message: 'impossible de recuperer ce sang' });
             } finally {
-                await prisma.$disconnect
+                await prisma.$disconnect();
             }
-
             break;
 
-        case 'PATCH': 
-
-            const { bloodGroupe, quantity, typeproduit } = req.body
+        case 'PATCH':
+            const { bloodGroupe, quantity, typeproduit } = req.body;
 
             try {
                 const blood = await prisma.blood.update({
@@ -40,38 +35,35 @@ export default async function handler(req, res) {
                         quantity: quantity,
                         productType: typeproduit
                     }
-                })
+                });
 
-                res.status(200).json({blood})
-
+                res.status(200).json({ blood });
             } catch (error) {
-                res.status(404).json({message: 'impossible de mettre a jour cette poche de sang'})
+                res.status(404).json({ message: 'impossible de mettre a jour cette poche de sang' });
             } finally {
-                await prisma.disconnect
+                await prisma.$disconnect();
             }
-
             break;
 
-        case 'DELETE': 
+        case 'DELETE':
             try {
                 const blood = await prisma.blood.delete({
                     where: {
                         id: req.query.id_blood
                     }
-                })
-                
-                res.status(200).json({blood})
+                });
 
+                res.status(200).json({ blood });
             } catch (error) {
-                console.log(error)
-                res.status(404).json({message: 'impossible de supprimer cette poche de sang'})
+                console.log(error);
+                res.status(404).json({ message: 'impossible de supprimer cette poche de sang' });
             } finally {
-                await prisma.$disconnect
+                await prisma.$disconnect();
             }
             break;
-    
+
         default:
-            res.status(401).json({message: `La methode ${methode} n'est pas autorisee !`})
+            res.status(401).json({ message: `La methode ${method} n'est pas autorisee !` });
             break;
     }
 }
